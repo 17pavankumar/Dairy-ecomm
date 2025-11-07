f31d68a776d1b70c4645eac557e2bd7b19987615
17pavankumar <17pavankumarn@gmail.com>
2025-11-07 09:11:42 +0530

    added address page

--- app/urls.py
+++ app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("category-title/<slug:val>", views.CategoryTitle.as_view(),name="Category-title"),
     path("product-detail/<int:pk>", views.ProductDetail.as_view(),name="product-detail"),
     path('profile/',views.ProfileView.as_view(),name='profile'),
+    path('address/',views.address,name='address'),
     
     #login authentication
     path("registration/", views.CustomerRegistrationView.as_view() , name='customerregistration'),
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from app.models import Product
+from app.models import Product ,Customer
 from app.forms import CustomerRegistrationForm ,CustomerProfileForm
 from django.contrib import messages
 # Create your views here.
@@ -50,5 +50,26 @@ class ProfileView(View):
     def get(self,request):
         form = CustomerProfileForm()
         return render(request , 'app/profile.html',locals())
-    def get(self,request):
+    def post(self,request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user 
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']\
+                
+                
+            reg = Customer(user=user , name=name , locality=locality , mobile=mobile , city=city , state=state ,zipcode=zipcode)
+            reg.save()
+            messages.success(request , "congratulations! profile save successfully")
+        else:
+            messages.warning(request , "invalid input data")
         return render(request , 'app/profile.html',locals())
+    
+
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request , 'app/address.html',locals())
